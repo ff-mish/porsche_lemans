6d13ca98db7e2d2b1f34c67802c30ca20c605426
hdg <hdg1988@gmail.com>
2014-05-19 02:08:39 +0800

    fix

--- web/js/lp.base.js
+++ web/js/lp.base.js
@@ -880,18 +880,50 @@ LP.use(['jquery', 'api', 'easing'] , function( $ , api ){
     });
 
     LP.action('preview' , function( ev ){
+
         // show big pic or big video
+        var tpls = {
+            'video': '<div class="popup_fuel">\
+                    <div class="popup_close"></div>\
+                    <div class="popup_fuel_video">\
+                        <h4>Memories of Porsche in Le Mans</h4>\
+                        <div class="popup_image_wrap"><img src="#[imgsrc]"/></div>\
+                    </div>\
+                    <div class="popup_fuel_btns">\
+                        <a class="repost" href="#">Repost</a>\
+                    </div>\
+                </div>',
+            'image': '<div class="popup_fuel" >\
+                <div class="popup_close"></div>\
+                <div class="popup_fuel_photo_left">\
+                    <div class="popup_image_wrap"><img src="#[imgsrc]"/></div>\
+                </div>\
+                <div class="popup_fuel_photo_right">\
+                    <h4>1979 Racing Poster</h4>\
+                    <div class="popup_fuel_photo_description">\
+                        1979 goes down in Porsche motorsport history as another successful year. Private racing teams bring home 15 national and international championship victories for the Zuffenhausen brand. One is the triumph in Le Mans. The Kremer Racing team from Cologne takes to the starting line in a modified version of the 935. Brothers Bill and Don Whittington from the United States and the German Klaus Ludwig win Porsche\'s fifth overall victory after 24 hours.\
+                    </div>\
+                    <div class="popup_fuel_btns">\
+                        <a class="repost" href="#">Repost</a>\
+                    </div>\
+                </div>\
+                <div class="cs-clear"></div>\
+            </div>'
+        }
+
         var $img = $(this)
             .closest('.fuelitem')
             .children('img');
-        var imgH = $img.height();
-        var imgW = $img.width();
+        // var imgH = $img.height();
+        // var imgW = $img.width();
         var video = $(this).closest('.fuelitem').data('video');
+
+        var content = LP.format(video ? tpls['video'] : tpls['image'] , {imgsrc: $img.attr('src')});
         LP.panel({
-            content: "<div class='fuel-content-wrap' style='overflow:hidden;'><img src=\"" + $img.attr('src') + "\"/></div>",
+            content: content,
             //title: "share the content",
             title: '',
-            submitButton: true,
+            width: 900,
             onShow: function(){
                 this.$panel.find('.lpn_panel')
                     .css({
@@ -902,11 +934,11 @@ LP.use(['jquery', 'api', 'easing'] , function( $ , api ){
                         marginTop: 0,
                         opacity: 1
                     } , 500 , 'easeOutQuart' , function(){
-                        var imgH = $('.fuel-content-wrap img').height();
-                        var imgW = $('.fuel-content-wrap img').width();
+                        var imgH = $('.popup_image_wrap img').height();
+                        var imgW = $('.popup_image_wrap img').width();
                         if( video ){ // play the video
-                            $('.fuel-content-wrap img').hide();
-                            renderVideo( $('.fuel-content-wrap').css({width: imgW , height: imgH}) , video.replace(/\.\w+$/ , '') , $img.attr('src') ,  {} );
+                            $('.popup_image_wrap img').hide();
+                            renderVideo( $('.popup_image_wrap').css({width: imgW , height: imgH}) , video.replace(/\.\w+$/ , '') , $img.attr('src') ,  {} );
                         }
 
                         $(window).on('resize.fixfuel' , function(){
@@ -922,11 +954,16 @@ LP.use(['jquery', 'api', 'easing'] , function( $ , api ){
                             }
 
                             // resize the panel
-                            panel.resize( tarW , tarH );
+                            //panel.resize( tarW , tarH );
                         });
                     } );
 
                 var panel = this;
+
+                this.$panel.find('.popup_close')
+                    .click(function(){
+                        panel.close();
+                    });
             },
             onBeforeClose: function(){
                 var $panel = this.$panel;
