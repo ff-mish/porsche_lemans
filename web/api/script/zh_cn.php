548d7f5e4238c99fc63e2746b6e67e4fae81cb7e
hdg <hdg1988@gmail.com>
2014-05-29 14:16:30 +0800

    fix

---
+++ web/api/script/zh_cn.php
@@ -0,0 +1,22 @@
+<?php 
+ return array (
+  'Repost' => '',
+  'Cancel' => '',
+  'Impact' => '',
+  'Quality' => '',
+  'Speed' => '',
+  'Assiduity' => '',
+  'Receive a reminder for the d-day' => '',
+  'Enter your email address' => '',
+  'Finish tutorial' => '',
+  'Success!' => '',
+  'wrong email' => '',
+  'Ok' => '',
+  'Send Invitation' => '',
+  ' You can\\\'t invite too many people ' => '',
+  'Cancel Invitation' => '',
+  'Confirm' => '',
+  'They’re watching you! A NEW psychological thriller from @kevwilliamson starring @DylanMcDermott &amp; @MaggieQ Wed 10/9c pic.twitter.com/o5v4b7M2is' => '',
+  'do you want to leave the team?' => '',
+  'Leave Team' => '',
+);
