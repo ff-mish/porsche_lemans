f655ea09c6b195873e0197a4045c5af133c5279b
Jackey <jziwenchen@gmail.com>
2014-06-13 16:24:22 +0800

    404

--- web/api/protected/views/error/404.php
+++ web/api/protected/views/error/404.php
@@ -1,1 +1,11 @@
-404 not found
+  <!--  -->
+  <div class="page pagebg404">
+        <!--  -->
+        <div class="header">
+            <div class="logo"><?php echo Yii::t("lemans", "PORSCHE")?></div>
+	        <a target="_blank" href="<?php echo Yii::t("lemans", "PORSCHE_LINK")?>" class="hd_info"></a>
+        </div>
+        
+        
+  </div>
+  <!--  -->
