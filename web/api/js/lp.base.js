88bb4bbe78a20834301498ee1b9ab474254bab80
fuel <gif@fuel-it-up.com>
2014-06-14 21:12:14 +0800

    update

--- web/api/js/lp.base.js
+++ web/api/js/lp.base.js
@@ -3615,7 +3615,7 @@ LP.use(['jquery', 'api', 'easing', 'queryloader', 'transit'] , function( $ , api
                         noClickClose = false;
                     }
                     else {
-                        $()
+                        $('.footer_logout').hide();
                     }
 
 
@@ -3628,6 +3628,7 @@ LP.use(['jquery', 'api', 'easing', 'queryloader', 'transit'] , function( $ , api
                             var $wrap = panel.$panel.find('.popup_image_wrap');
                             if(TYPE == 'image') {
                                 renderImage( $('.popup_image_wrap') , VIDEO , true , 0 , true );
+                                $('.popup_image_wrap').css({'text-align':'left'});
                             }
                             else {
                                 renderVideo( $('.popup_image_wrap')/*.css({width: imgW , height: imgH + 30})*/ , VIDEO.replace(/\.\w+$/ , '') , POSTER ,  {
