26fc80b77b44d8e0ec756d67c37112e56ca60bf9
Jacky chen <jziwenchen@gmail.com>
2014-11-17 15:23:24 +0800

    Update config.php

--- web/api/protected/extensions/sinaWeibo/config.php
+++ web/api/protected/extensions/sinaWeibo/config.php
@@ -1,4 +1,4 @@
 <?php
-define( "WB_AKEY" , '1910934872' );
-define( "WB_SKEY" , '56d5e1706993cb29ad250feb05ed7362' );
-define( "WB_CALLBACK_URL" , 'http://lemans.local/api/sns/weibocallback' );
+define( "WB_AKEY" , '2999614459' );
+define( "WB_SKEY" , '9c925b1a331847acfd1a4fa144a963a2' );
+define( "WB_CALLBACK_URL" , 'http://24socialrace.ffshtest.net/api/sns/weibocallback' );
